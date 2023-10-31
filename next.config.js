474ca9c495c8456b68b41b00af3492b190131a06
Jimmy Nguyen <son.sosc@carehealth.io>
2023-10-31 17:45:46 +0700

    update ip

--- next.config.js
+++ next.config.js
@@ -8,10 +8,11 @@ const nextConfig = {
     API_HASURA: process.env.API_HASURA,
     PASS_HASURA: process.env.PASS_HASURA,
     API_NODE: process.env.API_NODE,
+    FACEBOOK_API: process.env.FACEBOOK_API,
     JWT_SECRET_KEY: process.env.JWT_SECRET_KEY
   },
   images: {
-    domains: ["cdn.shopify.com"]
+    domains: ['cdn.shopify.com']
   }
 };
 
